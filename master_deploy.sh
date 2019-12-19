c4c237d39a1d82f63856397e044bcbab88f5ab52
Viraat Das <viraat.laldas@gmail.com>
2019-12-18 20:13:00 -0500

    update

--- master_deploy.sh
+++ master_deploy.sh
@@ -1,4 +1,10 @@
+git add .
+git commit -m "$1"
+git push -u origin development
 git checkout master
 git checkout development -- dist/
 mv dist/* .
 rm -rf dist
+git add .
+git commit -m "$1"
+git push -u origin master
